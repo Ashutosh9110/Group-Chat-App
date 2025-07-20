acc26f974b5e2be63dbe815fc7ddd16abfeae5a9
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-07-20 13:02:37 +0530

    Creating Signup Screen Backend Apis

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const router = express.Router()
+const userController = require("../controller/userController")
+
+// router.get("/", userController.signUp)
+router.post("/signup", userController.signUp)
 
 
 
